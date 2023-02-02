9d890fc51cd714dce06575e152a1ff21fea3896a
dtchoffonz <dilandtchoffo@gmail.com>
2023-02-02 17:35:12 +0100

    Estimateur sans standarisation des variables

---
+++ Estimateur.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+import numpy as np
+
+df = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv', delimiter=';')
+
+data_set = np.array(df, dtype=np.float)
+
+target = data_set[:, -1] # variable cible
+
+data = data_set[:, :-1]
+
+# Début de la programmation de l'estimateur des moindres carrés "beta"
+
+dataTxtarget = np.dot(data.T, target) #produit matriciel en la transposée de data et target
+
+datainv = np.linalg.inv(np.dot(data.T, data))
+
+beta = np.dot(datainv, dataTxtarget)
+
+#Fin de la programmmation de l'estimateur des moindres carrés "beta"
+
+int(np.sum((np.dot(data, beta) - target)**2)) #Calcul des résidus
---
+++ README.md
@@ -0,0 +1,3 @@
+# Estimateur des moindres carrés et calcul des résidus
+Ce projet consistait à programmer un estimateur, en l'occurence celui des moindres carrés, en utilisant les librairies Pandas et Numpy de Python.
+Lien de la base de données que nous avons utilisé : http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv
